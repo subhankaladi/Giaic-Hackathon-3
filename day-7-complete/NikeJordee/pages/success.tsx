c5c8346e78b9d45cd8d9533c73359dcd69ac45dc
subhankaladi <subhankaladi@gmail.com>
2025-01-28 19:32:35 +0500

    Complete Stripe

--- day-7-complete/NikeJordee/pages/success.tsx
+++ day-7-complete/NikeJordee/pages/success.tsx
@@ -1,25 +1,17 @@
-
 import Link from 'next/link';
-import './success.css'
-
+import './success.css';
 
 const SuccessStripe = () => {
-
   return (
     <div className="success-page">
       <div className="success-container">
         <h1>Order Successfully Placed!</h1>
         <div className="success-details">
-          <h2>
-            Thank You,
-            </h2>
+          <h2>Thank You,</h2>
           <p>Your order has been successfully placed and is now being processed.</p>
-        
         </div>
-        <Link
-        href={"/"}
-        >
-          Back to Homepage
+        <Link href={"/"}>
+         Back to Homepage
         </Link>
       </div>
     </div>
